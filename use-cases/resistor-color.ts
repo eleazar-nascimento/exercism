b64d3c24c39456df1d2ed941d96fd9cf9fc940c3
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-04-29 09:10:14 -0300

    feat: implement resistor-color solution

---
+++ use-cases/resistor-color.ts
@@ -0,0 +1,31 @@
+const objectColorsValue: any = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9,
+}
+
+export const colorCode = (color: string) => {
+  const includeColor = objectColorsValue[color]
+  
+  return includeColor
+}
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
